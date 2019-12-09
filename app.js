d5b85e02e7377b7965a939a54cb306f8d2912b70
Wimukdakom <SXW8888@yum.com>
2019-12-09 15:46:41 -0600

    Added individual status changer POST call

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const app = express();
 const snooze = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const waitTimeMs = process.env.WAIT_TIME;
 
-let status = 201
+let statuses = {
+  track: 201,
+  exportIds: 201,
+  aliases: 201,
+  subscription: 201
+}
 
 const processRequestBody = new ProcessRequestBody();
 
@@ -25,24 +30,24 @@ app.post('/users/track', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.profileRequest(req.body)
 
-  if (status === 201 ) res.status(status).send({message: 'success'});
-  else res.status(status).send({message: `/USER/TRACK Error ${status}`});
+  if (statuses.track === 201 ) res.status(statuses.track).send({message: 'success'});
+  else res.status(statuses.track).send({message: `/USER/TRACK Error ${statuses.track}`});
 });
 
 app.post('/users/alias/new', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.aliasRequest(req.body)
 
-  if (status === 201 ) res.status(status).send({message: `/ALIASES created ${status}`});
-  else res.status(status).send({message: `/ALIASES Error ${status}`});
+  if (statuses.aliases === 201 ) res.status(statuses.aliases).send({message: `+++++++++++++++ /ALIASES created ${statuses.aliases}`});
+  else res.status(statuses.aliases).send({message: `+++++++++++++++ /ALIASES Error ${statuses.aliases}`});
 });
 
 app.post('/subscription/status/set', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.smsRequest(req.body)
 
-  if (status === 201 ) res.status(status).send({message: `/SMS created ${status}`});
-  else res.status(status).send({message: `/SMS Error ${status}`});
+  if (statuses.subscription === 201 ) res.status(statuses.subscription).send({message: `+++++++++++++++ /SMS created ${statuses.subscription}`});
+  else res.status(statuses.subscription).send({message: `+++++++++++++++ /SMS Error ${statuses.subscription}`});
 });
 
 app.post('/users/export/ids', async (req, res, next) => {
@@ -55,16 +60,17 @@ app.post('/users/export/ids', async (req, res, next) => {
           }
         ]
   }
-  if (status === 201 ) res.status(status).json(mockResponse);
-  else res.status(status).send({message: `/USERS/EXPORT/IDS Error ${status}`});
+  if (statuses.exportIds === 201 ) res.status(statuses.exportIds ).json(mockResponse);
+  else res.status(statuses.exportIds).send({message: `/USERS/EXPORT/IDS Error ${statuses.exportIds}`});
 });
 
 app.post('/admin', async (req, res, next) => {
-  if(req.body.status) {
-    console.log(`Setting endpoints status From ${status} to ${req.body.status}`);
-    status = parseInt(req.body.status, 10);
-    res.send(`Status changed to ${status}`);
-  }
+  console.log(`Setting endpoints status From ${JSON.stringify(statuses)} to ${JSON.stringify(req.body)}`);
+  if (req.body.track) statuses.track = parseInt(req.body.track, 10);
+  if (req.body.exportIds) statuses.exportIds = parseInt(req.body.exportIds, 10);
+  if (req.body.aliases) statuses.aliases = parseInt(req.body.aliases, 10);
+  if (req.body.subscription) statuses.subscription = parseInt(req.body.subscription, 10);
+  res.send(`Status changed to ${JSON.stringify(statuses)}`);
 });
 
 
