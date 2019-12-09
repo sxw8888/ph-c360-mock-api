6ff90a14daf5004ab85e03a30466a6d1faeaa996
Wimukdakom <SXW8888@yum.com>
2019-12-09 14:30:55 -0600

    Added endpoint status changer call

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 const snooze = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const waitTimeMs = process.env.WAIT_TIME;
 
+let status = 201
+
 const processRequestBody = new ProcessRequestBody();
 
 if (process.env.ENVIRONMENT === 'dev') app.use(morgan('dev'));
@@ -23,24 +25,24 @@ app.post('/users/track', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.profileRequest(req.body)
 
-  res.status(201).send({message: 'success'});
-  // res.status(429).send({message: '/USER/TRACK Error 429'});
+  if (status === 201 ) res.status(status).send({message: 'success'});
+  else res.status(status).send({message: `/USER/TRACK Error ${status}`});
 });
 
 app.post('/users/alias/new', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.aliasRequest(req.body)
 
-  res.status(201).send({message: '/ALIASES created 201'});
-  // res.status(429).send({message: '/ALIASES Error 429'});
+  if (status === 201 ) res.status(status).send({message: `/ALIASES created ${status}`});
+  else res.status(status).send({message: `/ALIASES Error ${status}`});
 });
 
 app.post('/subscription/status/set', async (req, res, next) => {
   await snooze(waitTimeMs);
   processRequestBody.smsRequest(req.body)
 
-  res.status(201).send({message: '/SMS created 201'});
-  // res.status(429).send({message: '/SMS Error 429'});
+  if (status === 201 ) res.status(status).send({message: `/SMS created ${status}`});
+  else res.status(status).send({message: `/SMS Error ${status}`});
 });
 
 app.post('/users/export/ids', async (req, res, next) => {
@@ -53,8 +55,16 @@ app.post('/users/export/ids', async (req, res, next) => {
           }
         ]
   }
-  res.status(201).json(mockResponse);
-  // res.status(429).send({message: '/SMS Error 429'});
+  if (status === 201 ) res.status(status).json(mockResponse);
+  else res.status(status).send({message: `/USERS/EXPORT/IDS Error ${status}`});
+});
+
+app.post('/admin', async (req, res, next) => {
+  if(req.body.status) {
+    console.log(`Setting endpoints status From ${status} to ${req.body.status}`);
+    status = parseInt(req.body.status, 10);
+    res.send(`Status changed to ${status}`);
+  }
 });
 
 
